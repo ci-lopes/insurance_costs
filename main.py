2ab711473954739e07e46e1663aca5109c5f41da
ci-lopes <48160651+ci-lopes@users.noreply.github.com>
2022-01-20 16:21:48 +0000

    initial

--- main.py
+++ main.py
@@ -1,47 +1,9 @@
 import csv
+from costumer import Costumer
 
 ## csv columns: age,sex,bmi,children,smoker,region,charges
 
-## create Costumer class
-class Costumer:
-    def __init__(self, identifier, age, sex, bmi, children, smoker, charges):
-        self.identifier = identifier
-        self.age = age
-        self.sex = sex
-        self.bmi = bmi
-        self.children = children
-        self.smoker = smoker
-        self.charges = charges
-        self.estimated = self.estimated_insurance_cost()
-        self.diff = self.diff_charged_estimated_costs()
-
-    def __repr__(self):
-        return "The id of the current costumer is: {id}.".format(id = self.identifier)
-    
-    def estimated_insurance_cost(self):
-        return round(250 * self.age - 128 * self.sex + 370 * self.bmi + 425 * self.children + 24000 * self.smoker - 12500 , 1)
-        
-    def charged_insurance_cost(self):
-        return self.charges
-
-    def diff_charged_estimated_costs(self):
-        return round(self.charges - self.estimated, 1)
-        
-    def has_insurance(self):
-        if self.charges > 0:
-            return "The costumer id:{id} has a medical insurance.".format(id = self.identifier)
-        else:
-            return "The costumer id:{id} does not have a medical insurance.".format(id = self.identifier)
-
-    def export_data(self):
-        return "{identifier},{age},{sex},{bmi},{children},{smoker},{charges},{estimated},{diff}\n".format(
-            identifier = self.identifier, age = self.age, sex = self.sex, bmi = self.bmi, 
-            children = self.children, smoker = self.smoker, charges = self.charges, 
-            estimated = self.estimated, diff = self.diff      
-            )
-
 ## test Costumer methods     
-# instanciate Costumer   
 costumer1 = Costumer(0, 30, 1 , 23.2, 0, 0, 1300)
 print(costumer1)
 print(costumer1.estimated)
@@ -80,9 +42,16 @@ with open("insurance.csv") as insurance_data:
     for row in reader:
         costumer = Costumer(identifier, int(row["age"]), convert_sex_data(row["sex"]), float(row["bmi"]), 
         int(row["children"]), convert_smoker_data(row["smoker"]), round(float(row["charges"]),1))
-        print(costumer.age)
+        #print(costumer.diff)
         costumer_data.append(costumer)
         identifier = identifier + 1
+
+
+
+
+
+
+
 
 
 
