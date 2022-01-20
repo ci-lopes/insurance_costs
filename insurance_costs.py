224beecabb75a78492eb8ef8c576676aa3e899d5
ci-lopes <48160651+ci-lopes@users.noreply.github.com>
2022-01-20 15:35:27 +0000

    final project files

--- insurance_costs.py
+++ insurance_costs.py
@@ -12,36 +12,43 @@ class Costumer:
         self.children = children
         self.smoker = smoker
         self.charges = charges
-        
+        self.estimated = self.estimated_insurance_cost()
+        self.diff = self.diff_charged_estimated_costs()
+
     def __repr__(self):
         return "The id of the current costumer is: {id}.".format(id = self.identifier)
     
     def estimated_insurance_cost(self):
-        self.estimated = 250 * self.age - 128 * self.sex + 370 * self.bmi + 425 * self.children + 24000 * self.smoker - 12500
-        return self.estimated
-    
+        return round(250 * self.age - 128 * self.sex + 370 * self.bmi + 425 * self.children + 24000 * self.smoker - 12500 , 1)
+        
     def charged_insurance_cost(self):
         return self.charges
 
     def diff_charged_estimated_costs(self):
-        self.diff = self.charges - self.estimated
-        return self.diff
-    
+        return round(self.charges - self.estimated, 1)
+        
     def has_insurance(self):
         if self.charges > 0:
             return "The costumer id:{id} has a medical insurance.".format(id = self.identifier)
         else:
             return "The costumer id:{id} does not have a medical insurance.".format(id = self.identifier)
 
+    def export_data(self):
+        return "{identifier},{age},{sex},{bmi},{children},{smoker},{charges},{estimated},{diff}\n".format(
+            identifier = self.identifier, age = self.age, sex = self.sex, bmi = self.bmi, 
+            children = self.children, smoker = self.smoker, charges = self.charges, 
+            estimated = self.estimated, diff = self.diff      
+            )
+
 ## test Costumer methods     
 # instanciate Costumer   
 costumer1 = Costumer(0, 30, 1 , 23.2, 0, 0, 1300)
 print(costumer1)
-print(costumer1.estimated_insurance_cost())
+print(costumer1.estimated)
 print(costumer1.charges)
-print(costumer1.diff_charged_estimated_costs())
+print(costumer1.diff)
 print(costumer1.has_insurance())
-
+print(costumer1.export_data())
 
 
 ## convert sex and smoker columns to bool
@@ -63,14 +70,49 @@ def convert_smoker_data(smoker_data):
         return smoker_data
 #print(convert_smoker_data("no"))
 
+
 ## open csv file and read it as a dict 
+# create costumer_data variable that stores all costumers data
+costumer_data = []
 with open("insurance.csv") as insurance_data:
     reader = csv.DictReader(insurance_data)
     identifier = 0
     for row in reader:
-        costumer = Costumer(identifier, row["age"], convert_sex_data(row["sex"]), row["bmi"], 
-        row["children"], convert_smoker_data(row["smoker"]), row["charges"])
-        #print(costumer.charges)
-        
+        costumer = Costumer(identifier, int(row["age"]), convert_sex_data(row["sex"]), float(row["bmi"]), 
+        int(row["children"]), convert_smoker_data(row["smoker"]), round(float(row["charges"]),1))
+        #print(costumer.diff)
+        costumer_data.append(costumer)
         identifier = identifier + 1
 
+
+## returns estimated insurance costs by costumer from a list of costumers
+def estimated_insurance_cost_by_costumer(costumers_list):
+    for costumer in costumers_list:
+        print(costumer.estimated)
+
+## function call
+#estimated_insurance_cost_by_costumer(costumer_data)
+
+
+## returns difference between charged and estimated insurance cost by costumer from a list of costumers
+def diff_insurance_cost_by_costumer(costumers_list):
+    for costumer in costumers_list:
+        print(costumer.diff)
+
+## funcation call
+#diff_insurance_cost_by_costumer(costumer_data)
+
+
+## write new csv with updated data along with the estimated insurance cost and diff between charged and estimated costs by costumer
+with open("insurance_data_final.csv", "w") as output_csv:
+    fieldnames = "id,age,sex,bmi,children,smoker,charges,estimated_insurance_cost,difference\n"
+    output_csv.write(fieldnames)
+    for costumer in costumer_data:
+        output_csv.write(costumer.export_data())
+
+## read output csv
+with open("output.csv") as output_csv:
+    print(output_csv.read())
+    reader = csv.DictReader(output_csv)
+    for row in reader:
+        print(row)
